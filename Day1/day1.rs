8928f43ef02b5c4636334923cde1e6f0c4a3c2ab
Andrew Lonsway <ajlonsway@git.sep.com>
2019-12-19 15:13:30 -0500

    Finished part 2

--- Day1/day1.rs
+++ Day1/day1.rs
@@ -7,10 +7,10 @@ fn main() {
     let path = Path::new("input.txt");
 
     let mut file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", why.description()),
+        Err(why) => panic!("couldn't open: {}", why.description()),
         Ok(file) => file,
     };
-
+	
     let mut s = String::new();
     match file.read_to_string(&mut s) {
         Err(why) => panic!("couldn't read: {}", why.description()),
@@ -22,7 +22,7 @@ fn main() {
 				if *word != "" {
 				
 					match word.parse::<f64>() {
-						Ok(n) =>  sum = sum + ((n / 3.0).floor() - 2.0),
+						Ok(n) =>  sum = sum + fuel_calculator(n),
 						Err(e) => panic!("bad conversion {}", e.description()),
 					}
 				}
@@ -31,4 +31,13 @@ fn main() {
 			println!("Total sum: {}", sum);
 		}
     }
+}
+
+fn fuel_calculator(n : f64) -> f64 {
+	let result = (n / 3.0).floor() - 2.0;
+	if result <= 0.0 {
+		return 0.0;
+	} else {
+		return result + fuel_calculator(result);
+	}
 }
